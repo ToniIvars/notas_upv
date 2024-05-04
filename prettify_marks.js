cf53f79998e4da092011ca0f95e606923675d0f4
Toni Ivars <toniivaju@gmail.com>
2024-05-04 11:45:54 +0200

    Add chechbox to sort the marks or go back to the original sorting

--- prettify_marks.js
+++ prettify_marks.js
@@ -1,8 +1,11 @@
-function sortMarks() {
-  const table = document.querySelector('table')
-  const trs = [...table.querySelector('tbody').querySelectorAll('tr')]
+const notasUPVElement = '<div style="text-align: center; margin-bottom: 6px;"><span style="color: #ff524e;">Notas UPV</span><input type="checkbox" id="notasupv-check" checked></div>'
 
-  let elements = trs.map(tr => {
+const table = document.querySelector('table')
+const trs = [...table.querySelector('tbody').querySelectorAll('tr')]
+const { elements, sorted_elements } = getElements()
+
+function getElements() {
+  let sorted_elements = trs.map(tr => {
     let tds = tr.querySelectorAll('td')
     let name = tds[0].textContent
     let mark = tds[1].textContent.replace(',', '.')
@@ -11,33 +14,41 @@ function sortMarks() {
     return { name, mark }
   })
 
-  elements.sort((a, b) => b.mark - a.mark || b.name - a.name)
+  let elements = [...sorted_elements]
+  sorted_elements.sort((a, b) => b.mark - a.mark || b.name - a.name)
 
+  return { elements, sorted_elements }
+}
+
+function changeTable(elements) {
   trs.forEach((tr, i) => {
     let name_td = document.createElement('td')
-    name_td.classList.add('alignright')
+    name_td.classList.add('alignleft')
     name_td.textContent = elements[i].name
 
     let mark_td = document.createElement('td')
     mark_td.classList.add('alignleft')
     mark_td.textContent = elements[i].mark.toString().replace('.', ',')
-    
+
     tr.replaceChildren(name_td, mark_td)
   })
 }
 
-function showNotasUPVElement() {
-  let infoNode = document.createElement('p')
-  infoNode.style.textAlign = 'center'
-  infoNode.style.color = '#ff524e'
-  infoNode.textContent = 'Notas UPV'
-  document.querySelector('.upv_containercontent').appendChild(infoNode)
+function setupNotasUPVElement() {
+  document.querySelector('.upv_containercontent').insertAdjacentHTML('beforebegin', notasUPVElement)
+  document.querySelector('#notasupv-check').addEventListener('change', event => {
+    if (event.target.checked) {
+      changeTable(sorted_elements)
+    } else {
+      changeTable(elements)
+    }
+  })
 }
 
 function start() {
-  sortMarks()
-  showNotasUPVElement()
-  console.info('Notas UPV')
+  setupNotasUPVElement()
+  changeTable(sorted_elements)
+  console.info('Notas UPV activo')
 }
 
 start()
